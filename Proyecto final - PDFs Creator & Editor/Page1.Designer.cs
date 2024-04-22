ce7d272aecc04d6fb363213d7d3547e4aa90019c
SIMON SANCHEZ SEPULVEDA <simon.sanchezs@udea.edu.co>
2024-04-21 22:28:32 -0500

    Creadas las paginas 1 y 2, conecté los botones de la primera pagina con su correspondiente dependencia, no los pude pobrar porque me sale un error de ubicacion de archivos al compilar.

---
+++ Proyecto final - PDFs Creator & Editor/Page1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Proyecto_final___PDFs_Creator___Editor
+{
+    partial class Page1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(290, 106);
+            button1.Name = "button1";
+            button1.Size = new Size(187, 59);
+            button1.TabIndex = 0;
+            button1.Text = "Ver historial";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(290, 238);
+            button2.Name = "button2";
+            button2.Size = new Size(187, 59);
+            button2.TabIndex = 1;
+            button2.Text = "Crear o editar PDF";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Page1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Page1";
+            Text = "Page1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Proyecto final - PDFs Creator & Editor/Page1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_final___PDFs_Creator___Editor
+{
+    public partial class Page1 : Form
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Page2 f1 = new Page2();
+            f1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Page4 f1 = new Page4();
+            f1.Show();
+        }
+    }
+}
