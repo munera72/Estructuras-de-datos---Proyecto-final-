96f99be56fe98a651a91001c87e67ef7a31e671e
Emanuel <122242669+munera72@users.noreply.github.com>
2024-04-22 21:53:37 -0500

    added some functionality for the home page empezar button

--- Proyecto final - PDFs Creator & Editor/Home.cs
+++ Proyecto final - PDFs Creator & Editor/Home.cs
@@ -11,6 +11,7 @@ namespace Proyecto_final___PDFs_Creator___Editor
         {
             Page1 f1 = new Page1();
             f1.Show();
+            this.Hide(); // Hide the current form
         }
     }
 }
